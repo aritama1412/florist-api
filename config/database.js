1a835a07477e4f1f9429d82e8316bb612c2b39fc
Edwin Aritama <aritamaedwin@gmail.com>
2025-02-03 22:30:28 +0700

    .

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("florist", "root", "", {
+const sequelize = new Sequelize("tes_florist", "root", "", {
   host: "localhost",
   dialect: "mysql",
 });
