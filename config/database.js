0517fcad708c29653350ad3c7ee719461a7fda38
Edwin Aritama <aritamaedwin@gmail.com>
2025-06-07 18:28:16 +0700

    .

--- config/database.js
+++ config/database.js
@@ -1,8 +1,7 @@
 const { Sequelize } = require("sequelize");
 
-// const sequelize = new Sequelize("tes_florist", "root", "", {
-const sequelize = new Sequelize("pondokd2_test_florist", "pondokd2_ica", "Inipassword", {
-  // host: "api.pondokdaun.beauty",
+const sequelize = new Sequelize("tes_florist", "root", "", {
+// const sequelize = new Sequelize("pondokd2_test_florist", "pondokd2_ica", "Inipassword", {
   host: "localhost",
   dialect: "mysql",
 });
